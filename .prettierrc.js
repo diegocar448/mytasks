1fadb65adb3f1ca59fd2bc81def02375ee88bf2f
Diego Cardoso <diegocar448@hotmail.com>
2024-02-21 09:13:56 -0300

    Prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
+  semi: true,
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
+  jsxBracketSameLime: true,
   singleQuote: true,
   trailingComma: 'all',
+  printWitdh: 80,
 };
