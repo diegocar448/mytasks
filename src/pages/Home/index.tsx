47392a2a510b62a6b54111767a0cce16b65c2fa7
Diego Cardoso <diegocar448@hotmail.com>
2024-02-22 10:34:04 -0300

    SafeAreaView

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView } from 'react-native';
 
 
 export const Home = () => /*:React.JSX.Element*/ {
   return (      
+    <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        <Text style={styles.title}>Ola dev!</Text>
+        <Text style={styles.title}>Fala dev!</Text>
       </View>
+    </SafeAreaView>
     )
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex:1,
+    backgroundColor:'#121214',    
+  },
   container: {
     flex:1,
     backgroundColor:'#121214',
-    justifyContent:'center', 
-    alignItems:'center',
+    paddingHorizontal:30,    
+    paddingVertical:50,    
   },
   title:{
     color: '#f1f1f1',
+    fontSize:24,
+    fontWeight:'bold',
   }
 });
