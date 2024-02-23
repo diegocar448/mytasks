02d9dfc0c1ce706aa8e477b9caada87615a5d53d
Diego Cardoso <diegocar448@hotmail.com>
2024-02-23 10:12:57 -0300

    CriandoContextoTasksContext

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 
 import React from 'react';
 import { Home } from './src/pages/Home';
+import { TasksContext } from './src/context/TasksContext';
 
 
 const App = () => /*:React.JSX.Element*/ {
   return (      
-      <Home />      
+      <TasksContext.Provider value={{ id: '1', title: 'Task01' }}>
+        <Home />  
+      </TasksContext.Provider>
+          
     )
 };
 
