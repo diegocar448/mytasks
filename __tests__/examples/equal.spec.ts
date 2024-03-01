99b151351ffccdc30a1e1fac5d339ef6a430d577
Diego Cardoso <diegocar448@hotmail.com>
2024-03-01 09:09:41 -0300

    toEqual_ToBe

---
+++ __tests__/examples/equal.spec.ts
@@ -0,0 +1,17 @@
+import React from 'react';
+import {it} from '@jest/globals';
+
+describe('Igualdade', () => {
+  it('uso basico - toEqual', () => {
+    expect(1+1).toEqual(2);
+    expect('developer').toEqual('developer');
+    expect({ name: 'developer' }).toEqual({ name: 'developer' });
+  });
+  it('uso basico - toBe', () => {
+    expect(1+1).toBe(2);
+    expect('developer').toBe('developer');
+    //expect({ name: 'developer' }).toBe({ name: 'developer' });
+  });
+});
+
+export {};
