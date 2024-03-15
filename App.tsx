c943329bc86592e6597460c6a6e8c26c51e486a3
Diego Cardoso <diegocar448@hotmail.com>
2024-03-15 14:46:45 -0300

    CriandoSplashScreenAndroidP2

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 
 import React from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import { Home } from './src/pages/Home';
 import { TasksProvider } from './src/context/TasksContext';
 
 
 
 const App = () => /*:React.JSX.Element*/ {
+
+  React.useEffect( () => {
+    SplashScreen.hide();
+  }, []);
   return (      
       <TasksProvider>
-        <Home />  
+        <Home />
       </TasksProvider>
           
     )
--- android/app/src/main/java/com/mytasks/MainActivity.kt
+++ android/app/src/main/java/com/mytasks/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.mytasks
-
+import org.devio.rn.splashscreen.SplashScreen
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  init {
+    SplashScreen.show(this)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
